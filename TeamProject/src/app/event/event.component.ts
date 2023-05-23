e6a2f1078935bbb7d7b87a506458ba9273a7022e
s6yv <55759727+s6yv@users.noreply.github.com>
2023-05-23 20:50:23 +0200

    Inital List of Events in Calendar

--- TeamProject/src/app/event/event.component.ts
+++ TeamProject/src/app/event/event.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import {EventInt} from '../event-int'
+import {EVENTLIST} from '../mock-events';
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.css']
 })
 export class EventComponent {
+  events = EVENTLIST;
 
 }
