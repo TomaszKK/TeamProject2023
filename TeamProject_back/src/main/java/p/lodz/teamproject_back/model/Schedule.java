6dc159ab89ffc9241a26d4ad06ed8e4e6b1a6524
TomaszKK <242136@edu.p.lodz.pl>
2023-05-23 21:20:49 +0200

    Some changes

--- TeamProject_back/src/main/java/p/lodz/teamproject_back/model/Schedule.java
+++ TeamProject_back/src/main/java/p/lodz/teamproject_back/model/Schedule.java
@@ -1,4 +1,24 @@
 package p.lodz.teamproject_back.model;
 
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+
 public class Schedule {
+    private Long id;
+    private String Date;
+    private String Week;
+
+    @OneToOne
+    private String User;
+
+    @ManyToOne
+    private Event Event;
+
+    public Schedule() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
 }
--- TeamProject_back/src/main/java/p/lodz/teamproject_back/repository/EventRepository.java
+++ TeamProject_back/src/main/java/p/lodz/teamproject_back/repository/EventRepository.java
@@ -1,4 +1,10 @@
 package p.lodz.teamproject_back.repository;
 
-public interface EventRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import p.lodz.teamproject_back.model.Event;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+    Event findById(String name);
 }
